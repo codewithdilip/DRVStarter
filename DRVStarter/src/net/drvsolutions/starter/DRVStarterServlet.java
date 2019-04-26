f7beb5ddc2097ea0ad92beb3e68bf2d88463a133
Gateway <Gateway@DilipPatel>
2019-04-26 18:33:29 +0530

    1

--- DRVStarter/src/net/drvsolutions/starter/DRVStarterServlet.java
+++ DRVStarter/src/net/drvsolutions/starter/DRVStarterServlet.java
@@ -11,13 +11,14 @@ import javax.servlet.http.HttpServletResponse;
 public class DRVStarterServlet extends HttpServlet{
 	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException,IOException{
 		res.setContentType("text/html");//setting the content type  
-		PrintWriter pw=res.getWriter();//get the stream to write the data  
+		//PrintWriter pw=res.getWriter();//get the stream to write the data  
 		  
 		//writing html in the stream  
-		pw.println("<html><body>");  
-		pw.println("Welcome to servlet");  
-		pw.println("</body></html>");  
-		  
-		pw.close();//closing the stream  
+		//pw.println("<html><body>");  
+		//pw.println("Welcome to servlet");  
+		//pw.println("</body></html>");  
+		
+		res.sendRedirect("index.html");  
+		//pw.close();//closing the stream  
 	}
 }
